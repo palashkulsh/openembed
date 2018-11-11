86cdb9c5f3e1343d2eda742e9c880f3fd18b3b59
palash-paytm <palash.kulshreshtha@paytm.com>
2018-11-11 14:23:10 +0530

    added status

--- routes/meta.js
+++ routes/meta.js
@@ -4,10 +4,15 @@ module.exports = {
   getmeta :function(req, res, next){
     urlMetadata(req.query.url).then(
       function (metadata) { // success handler
-        req.data = metadata;
+        req.data = {
+          data:metadata
+        };
+        req.data.status='success';
         return next();
       },
       function (error) { // failure handler
+        req.data = {};
+        req.data.status='failure';
         return next(error);
       });
   }
